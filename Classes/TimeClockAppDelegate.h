455ed4d1365e6023e9fdc025fbe9e0ef84cf17f3
mbaker000 <mbaker000@e99404b7-a951-4451-b7b0-258fdd885127>
2009-10-08 18:11:12 +0000

    Initial commit.  Currently working on JTCL-1
    git-svn-id: https://svn.8svn.com/mbaker000/svn/timeclock/trunk@2 e99404b7-a951-4451-b7b0-258fdd885127

---
+++ Classes/TimeClockAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  TimeClockAppDelegate.h
+//  TimeClock
+//
+//  Created by Matthew Baker on 10/8/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+@interface TimeClockAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
